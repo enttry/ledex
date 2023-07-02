22804c4484b8f6a12df6965f5b813f797b04baae
enttry <138381938+enttry@users.noreply.github.com>
2023-07-03 00:32:16 +0300

    Delete museim directory

--- museim/php/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Подключение к базе данных
-$conn = mysqli_connect("localhost", "root", "", "museim");
-// Проверка подключения
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
